14fb53760a9e83651ba80ec3d6684922dbfd947f
Erik Schierboom <erik_schierboom@hotmail.com>
2024-11-19 19:12:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Uiua language support for CodeMirror
+
+This repo contains the grammar for the [Uiua language](https://uiua-lang.io/).
---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import { parser } from "./syntax.grammar";
+import {
+  LRLanguage,
+  LanguageSupport,
+  indentNodeProp,
+  foldNodeProp,
+  foldInside,
+  delimitedIndent,
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@lezer/highlight";
+
+export const uiuaLanguage = LRLanguage.define({
+  name: "uiua",
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Application: delimitedIndent({ closing: ")", align: false }),
+      }),
+      foldNodeProp.add({
+        Application: foldInside,
+      }),
+      styleTags({
+        Word: t.variableName,
+        Logical: t.bool,
+        String: t.string,
+        LineComment: t.lineComment,
+        null: t.null,
+        Type: t.typeName,
+        Char: t.character,
+        "Integer Version": t.number,
+        Floating: t.float,
+        Color: t.atom,
+        "( )": t.paren,
+        "[ ]": t.squareBracket,
+        "{ }": t.brace,
+        Escape: t.escape,
+        Regex: t.regexp,
+        "Literal SymbolLiteral": t.atom,
+        Function: t.function(t.definition(t.variableName)),
+        Key: t.function(t.propertyName),
+        Symbol: t.atom,
+      }),
+    ],
+  }),
+  languageData: {
+    commentTokens: { line: ";" },
+  },
+});
+
+export function uiua() {
+  return new LanguageSupport(uiuaLanguage);
+}
